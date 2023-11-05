33c0dcd00c231d3d740cde87b0b2f78a4d3dd5a5
Mariia Kasianova <kasyanovamg@gmail.com>
2023-11-04 21:19:26 -0400

    added auth

--- final_project/index.js
+++ final_project/index.js
@@ -8,10 +8,58 @@ const app = express();
 
 app.use(express.json());
 
+let users = []
+
+const doesExist = (username)=>{
+  let userswithsamename = users.filter((user)=>{
+    return user.username === username
+  });
+  if(userswithsamename.length > 0){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+          // log in exsiting user
+          if (authenticatedUser(username,password)) {
+            let accessToken = jwt.sign({
+              data: password
+            }, 'access', { expiresIn: 60 * 60 });
+        
+            req.session.authorization = {
+              accessToken,username
+          }
+          return res.status(200).send("User successfully logged in");
+          } else {
+            return res.status(208).json({message: "Invalid Login. Check username and password"});
+          }
+        }
+    } 
+    return res.status(404).json({message: "Unable to authorize the user."});
+  
 });
  
 const PORT =5000;

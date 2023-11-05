35681827dc6f1b0aa28df87088fd7eea767e7ec4
Mariia Kasianova <kasyanovamg@gmail.com>
2023-11-05 00:33:26 -0400

    added promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -21,27 +21,39 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify(books,null,4));
-});
+    const get_books = new Promise((resolve, reject) => {
+        resolve(res.send(JSON.stringify({books}, null, 4)));
+      });
+      get_books.then(() => res.send(JSON.stringify(books,null,4)));
+  });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    const get_book = new Promise((resolve, reject) => {
+        resolve(res.send(books[isbn]));
+      });
+      get_book.then(() => res.send(JSON.stringify(books[isbn],null,4)));
 });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     const book = Object.values(books).find(book => book.author === author);
-    res.send(book)
+    const get_book = new Promise((resolve, reject) => {
+       resolve(res.send(book));
+      });
+      get_book.then(() => res.send(book));
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     const book = Object.values(books).find(book => book.title === title);
-    res.send(book)
+    const get_book = new Promise((resolve, reject) => {
+       resolve(res.send(book));
+      });
+      get_book.then(() => res.send(books));
 });
 
 //  Get book review
